ffe2751c233043625f38ba02cd5fc235ddfc21b3
carldanley <carldanley@gmail.com>
2014-10-06 03:28:30 -0400

    Cleaned up harvester.
    This commit cleans up harvester quite a bit by doing the following:
    
    1. Moves socket logic into a transport layer (so we can separate logic a
      little easier)
    1. Introduces a utilities file that can provide access to any utils we
      may need for harvester)
    1. Removes the `registerModule` command; this shouldn't be a public API
      function
    1. Introduces a filter system (through `addFilter`, `removeFilter`,
      `applyFilters`) so plugins and patcher modules can easily hook into
      any harvests.
    
    I also toyed with the idea of making transports a singleton but quickly
    decided against it because a developer should be able to instantiate as
    many harvesters as he wants in one application (though I personally
    wouldn't do this).

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // dependencies
-const patcher = require('./lib/patcher');
 const _ = require('lodash');
-const io = require('socket.io-client');
 const os = require('os');
 const extend = require('extend');
 const usage = require('usage');
 const bPromise = require('bluebird');
+const patcher = require('./lib/patcher');
+const utilities = require('./lib/utilities');
+const Transport = require('./lib/transport');
+
+// store a reference to the singleton socket connection
+var _socketConnection = null;
 
 function Harvester(opts) {
   // ensure we have (at the least) the default options we need
@@ -13,39 +17,26 @@ function Harvester(opts) {
     host: 'http://localhost:30000',
     name: 'Node Application'
   };
-  opts = extend({}, defaults, opts);
+  opts = extend({}, defaults, opts || {});
 
-  this.connected = false;
+  // internal storage
   this._crops = {};
+  this._filters = {};
   this._modules = {};
 
-  // environment
-  this.name = opts.name;
+  // setup a new transport so we can harvest properly
+  this._transport = new Transport({
+    host: opts.host
+  });
+  this._transport.on('connected', this._onTransportConnect.bind(this));
+  this._transport.on('disconnected', this._onTransportDisconnect.bind(this));
 
-  // socket
-  this.socket = io.connect(opts.host);
-  this.socket.on('connect', this._onSocketConnected.bind(this));
-  this.socket.on('disconnect', this._onSocketDisconnected.bind(this));
+  // app name
+  this.name = opts.name;
 
   // patch the loader so we can inject the harvester
   patcher.patchModuleLoader(this);
 }
-
-Harvester.prototype._getIPAddresses = function _getIP() {
-  var interfaces = os.networkInterfaces();
-  var addresses = [];
-
-  Object.keys(interfaces).forEach(function(name) {
-    Object.keys(interfaces[name]).forEach(function(property) {
-      var address = interfaces[name][property];
-      if (address.family === 'IPv4' && !address.internal) {
-        addresses.push(address.address);
-      }
-    });
-  });
-
-  return addresses;
-};
 
 Harvester.prototype.startHarvesting = function startHarvesting(name, interval, callback) {
   if (!_.isFunction(callback)) {
@@ -56,7 +47,7 @@ Harvester.prototype.startHarvesting = function startHarvesting(name, interval, c
   var harvest = _.partial(this.harvest.bind(this), name);
 
   // continue harvesting every `interval` of time
-  this._crops[name] = setInterval(function() {
+  this._crops[name] = setInterval(function performCropHarvest() {
     var result = callback();
 
     // handle promises that are passed to us
@@ -70,72 +61,65 @@ Harvester.prototype.startHarvesting = function startHarvesting(name, interval, c
 };
 
 Harvester.prototype.stopHarvesting = function stopHarvesting(name) {
-  if (this._crops[name]) {
-    clearInterval(this._crops[name]);
-    delete this._crops[name];
-  }
-};
-
-Harvester.prototype.harvest = function harvest(name, data) {
-  if (!this.connected) {
+  if (!this._crops[name]) {
     return;
   }
 
+  clearInterval(this._crops[name]);
+  delete this._crops[name];
+};
+
+Harvester.prototype.harvest = function harvest(name, data) {
   var harvestData = {
     appName: this.name,
     data: data
   };
 
-  this.socket.emit(name, harvestData);
+  this._transport.send(name, harvestData);
 };
 
-Harvester.prototype._onSocketConnected = function _onSocketConnected() {
-  this.connected = true;
+Harvester.prototype._onTransportConnect = function _onTransportConnect() {
+  // broadcast the environment data once (since we just connected)
+  this._harvestEnvironmentData();
 
-  // one-time harvesters
-  var cpus = os.cpus();
-  this.harvest('environment', {
-    title: process.title,
-    version: process.version,
-    architecture: process.arch,
-    platform: process.platform,
-    hostName: os.hostname(),
-    pid: process.pid,
-    ip: this._getIPAddresses(),
-    ram: (os.totalmem()/1024/1024/1024) + 'gb',
-    cpus: {
-      count: cpus.length,
-      type: (cpus.length > 0) ? cpus[0].model : 'unknown'
-    }
-  });
-
-  // interval harvesters
-  this.startHarvesting('health', 5000, this._monitorHealth.bind(this));
-  this.startHarvesting('process.info', 60000, this._monitorProcessInfo.bind(this));
+  // start our recurring harvest routines
+  this.startHarvesting('process.info', 5000, this._getProcessInfo.bind(this));
 };
 
-Harvester.prototype._onSocketDisconnected = function _onSocketDisconnected() {
-  this.connected = false;
-  this.stopHarvesting('health');
+Harvester.prototype._onTransportDisconnect = function _onTransportDisconnect() {
   this.stopHarvesting('process.info');
 };
 
-Harvester.prototype._monitorHealth = function _monitorHealth() {
+Harvester.prototype._harvestEnvironmentData = 
+  function _harvestEnvironmentData() {
+    var cpus = os.cpus();
+    var environmentData = {
+      title: process.title,
+      version: process.version,
+      architecture: process.arch,
+      platform: process.platform,
+      hostName: os.hostname(),
+      pid: process.pid,
+      ip: utilities.getIPAddresses(),
+      ram: (os.totalmem()/1024/1024/1024) + 'gb',
+      cpus: {
+        count: cpus.length,
+        type: (cpus.length > 0) ? cpus[0].model : 'unknown'
+      }
+    };
+
+    // allow other plugins to modify the environment data
+    environmentData = this.applyFilters('environment', environmentData);
+    this.harvest('environment', environmentData);
+  };
+
+Harvester.prototype._getProcessInfo = function _getProcessInfo() {
   var promises = {
     usage: this._getProcessUsage(),
-    modules: bPromise.resolve(this._modules)
+    modules: bPromise.resolve(this._modules),
+    uptime: bPromise.resolve(process.uptime())
   };
   return bPromise.props(promises);
-};
-
-Harvester.prototype._monitorProcessInfo = function _monitorProcessInfo() {
-  return {
-    uptime: process.uptime()
-  };
-};
-
-Harvester.prototype.registerModule = function registerModule(name, version) {
-  this._modules[name] = version;
 };
 
 Harvester.prototype._getProcessUsage = function _getProcessUsage() {
@@ -149,6 +133,64 @@ Harvester.prototype._getProcessUsage = function _getProcessUsage() {
       resolve(result);
     });
   });
+};
+
+Harvester.prototype.addFilter = 
+  function addFilter(name, callback, priority, context) {
+    if (!_.isString(name) || name.length === 0 ) {
+      throw new Error('Filter name must be a string with 1 or more characters');
+    } else if (!_.isFunction(callback)) {
+      throw new Error('Filter callback must be a valid function');
+    }
+
+    // ensure this filter name exists
+    if (!_.isArray(this._filters[name])) {
+      this._filters[name] = [];
+    }
+
+    // add the new filter and sort by priority
+    this._filters[name].push({
+      callback: callback,
+      priority: (_.isNumber(priority) ? priority : 10),
+      context: context || null
+    });
+    this._filters[name] = _.sortBy(this._filters[name], 'priority');
+};
+
+Harvester.prototype.removeFilter = 
+  function removeFilter(name, callback) {
+    if (!_.isString(name) || name.length === 0) {
+      throw new Error('Filter name must be a string with 1 or more characters');
+    } else if (!_.isFunction(callback)) {
+      throw new Error('Filter callback must be a valid function');
+    }
+
+    // remove any filters that were previously added with the specified callback
+    var filters = this._filters[name];
+    for (var i = filters.length; i--;) {
+      if (filters[i].callback === callback) {
+        this._filters[name].splice(i, 1);
+      }
+    }
+  };
+
+Harvester.prototype.applyFilters = function applyFilters(/* name, data */) {
+  var args = Array.prototype.slice.call(arguments);
+  var name = args.shift();
+  var data = args.shift();
+  var filters = this._filters[name];
+
+  if (!filters || filters.length === 0) {
+    return data;
+  }
+
+  _.each(filters, function(filter) {
+    var tmpArgs = args;
+    tmpArgs.unshift(data);
+    data = filter.callback.apply(filter.context, args);
+  });
+
+  return data;
 };
 
 exports = module.exports = Harvester;
